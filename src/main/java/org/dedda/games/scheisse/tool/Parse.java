d892affa164c37e45a71f4ffebca1bb280f41a87
Stephan Göppentin <dedda102@gmail.com>
2014-11-19 21:17:41 +0100

    used fqn in parse class for javadoc links

--- src/main/java/org/dedda/games/scheisse/tool/Parse.java
+++ src/main/java/org/dedda/games/scheisse/tool/Parse.java
@@ -5,8 +5,9 @@ import java.awt.Dimension;
 import java.awt.geom.Point2D;
 
 /**
- * Class for parsing {@link String}s to numbers or number based objects
- * such as {@link Dimension}s and casting numbers to a different type.
+ * Class for parsing {@link java.lang.String}s to numbers or number
+ * based objects such as {@link java.awt.Dimension}s and casting numbers
+ * to a different type.
  *
  * Created by dedda on 5/22/14.
  *
@@ -15,71 +16,73 @@ import java.awt.geom.Point2D;
 public abstract class Parse {
 
     /**
-     * Used for masking {@link Short} values to the length of a {@link Byte}.
+     * Used for masking {@link java.lang.Short} values to the length of a
+     * {@link java.lang.Byte}.
      */
     private static final short SHORT_TO_BYTE_MASK = 0x00FF;
 
     /**
-     * Used for masking {@link Integer} values to the length of a {@link Byte}.
+     * Used for masking {@link java.lang.Integer} values to the length of a
+     * {@link java.lang.Byte}.
      */
     private static final int INT_TO_BYTE_MASK = 0x000000FF;
 
     /**
-     * Parsing a {@link String} to an {@link Integer}.
+     * Parsing a {@link java.lang.String} to an {@link java.lang.Integer}.
      *
-     * @see Integer#parseInt(String)
+     * @see Integer#parseInt(java.lang.String)
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Integer}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.lang.Integer}
      */
     public static int toInteger(final String string) {
         return Integer.parseInt(string);
     }
 
     /**
-     * Parsing a {@link String} to a {@link Long}.
+     * Parsing a {@link java.lang.String} to a {@link java.lang.Long}.
      *
-     * @see Long#parseLong(String)
+     * @see Long#parseLong(java.lang.String)
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Long}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.lang.Long}
      */
     public static long toLong(final String string) {
         return Long.parseLong(string);
     }
 
     /**
-     * Parsing a {@link String} to a {@link Float}.
+     * Parsing a {@link java.lang.String} to a {@link java.lang.Float}.
      *
-     * @see Float#parseFloat(String)
+     * @see Float#parseFloat(java.lang.String)
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Float}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.lang.Float}
      */
     public static float toFloat(final String string) {
         return Float.parseFloat(string);
     }
 
     /**
-     * Parsing a {@link String} to a {@link Double}.
+     * Parsing a {@link java.lang.String} to a {@link java.lang.Double}.
      *
-     * @see Double#parseDouble(String)
+     * @see Double#parseDouble(java.lang.String)
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Double}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.lang.Double}
      */
     public static double toDouble(final String string) {
         return Double.parseDouble(string);
     }
 
     /**
-     * Parsing a {@link String} to a {@link Point}.
+     * Parsing a {@link java.lang.String} to a {@link java.awt.Point}.
      *
-     * Syntax for {@link Point}s in {@link String} representation looks
-     * like this: "locationX,locationY"
+     * Syntax for {@link java.awt.Point}s in {@link java.lang.String}
+     * representation looks like this: "locationX,locationY"
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Point}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.awt.Point}
      */
     public static Point toPoint(final String string) {
         int x = toInteger(string.substring(0, string.indexOf(',')));
@@ -88,13 +91,15 @@ public abstract class Parse {
     }
 
     /**
-     * Parsing a {@link String} to a {@link Point2D.Float}.
+     * Parsing a {@link java.lang.String} to a
+     * {@link java.awt.geom.Point2D.Float}.
      *
-     * Syntax for {@link Point2D.Float}s in {@link String} representation looks
+     * Syntax for {@link java.awt.geom.Point2D.Float}s in
+     * {@link java.lang.String} representation looks
      * like this: "locationX.xX,locationY.yY"
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Point2D.Float}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.awt.geom.Point2D.Float}
      */
     public static Point2D.Float toPoint2DFloat(final String string) {
         float x = toFloat(string.substring(0, string.indexOf(',')));
@@ -103,13 +108,15 @@ public abstract class Parse {
     }
 
     /**
-     * Parsing a {@link String} to a {@link Point2D.Double}.
+     * Parsing a {@link java.lang.String} to a
+     * {@link java.awt.geom.Point2D.Double}.
      *
-     * Syntax for {@link Point2D.Double}s in {@link String} representation looks
+     * Syntax for {@link java.awt.geom.Point2D.Double}s
+     * in {@link java.lang.String} representation looks
      * like this: "locationX.xX,locationY.yY"
      *
-     * @param string {@link String} to be parsed
-     * @return Parsed {@link Point2D.Double}
+     * @param string {@link java.lang.String} to be parsed
+     * @return Parsed {@link java.awt.geom.Point2D.Double}
      */
     public static Point2D.Double toPoint2DDouble(final String string) {
         double x = toDouble(string.substring(0, string.indexOf(',')));
@@ -118,13 +125,13 @@ public abstract class Parse {
     }
 
     /**
-     * Parsing a {@link String} to a {@link Boolean} value.
+     * Parsing a {@link java.lang.String} to a {@link java.lang.Boolean} value.
      *
-     * Every {@link String} that is not "1" or "true" evaluates to "false".
-     * Evaluation is case insensitive.
+     * Every {@link java.lang.String} that is not "1" or "true"
+     * evaluates to "false". Evaluation is case insensitive.
      *
-     * @param string {@link String} to parse
-     * @return Parsed {@link Boolean}
+     * @param string {@link java.lang.String} to parse
+     * @return Parsed {@link java.lang.Boolean}
      */
     public static boolean toBoolean(final String string) {
         if (string.equals("1") || string.toLowerCase().equals("true")) {
@@ -134,13 +141,14 @@ public abstract class Parse {
     }
 
     /**
-     * Parsing a {@link String} to a {@link Dimension} value.
+     * Parsing a {@link java.lang.String} to a
+     * {@link java.awt.Dimension} value.
      *
-     * Syntax for {@link Dimension}s in {@link String} representation looks
-     * like this: "width,height"
+     * Syntax for {@link java.awt.Dimension}s in {@link java.lang.String}
+     * representation looks like this: "width,height"
      *
-     * @param string {@link String} to parse
-     * @return Parsed {@link Dimension}
+     * @param string {@link java.lang.String} to parse
+     * @return Parsed {@link java.awt.Dimension}
      */
     public static Dimension toDimension(final String string) {
         Point point = toPoint(string);
@@ -148,38 +156,40 @@ public abstract class Parse {
     }
 
     /**
-     * Casting a {@link Short} to a {@link Byte}.
+     * Casting a {@link java.lang.Short} to a {@link java.lang.Byte}.
      *
      * The left (most significant) 8 bit get cut off and the
-     * right (least significant) 8 bit are pushed in the {@link Byte}.
+     * right (least significant) 8 bit are pushed in the
+     * {@link java.lang.Byte}.
      *
-     * @param value {@link Short} to cast
-     * @return Casted {@link Byte}
+     * @param value {@link java.lang.Short} to cast
+     * @return Casted {@link java.lang.Byte}
      */
     public static byte toByte(final short value) {
         return (byte)((short) (value & SHORT_TO_BYTE_MASK));
     }
 
     /**
-     * Casting a {@link Integer} to a {@link Byte}.
+     * Casting a {@link java.lang.Integer} to a {@link java.lang.Byte}.
      *
      * The left (most significant) 24 bit get cut off and the
-     * right (least significant) 8 bit are pushed in the {@link Byte}.
+     * right (least significant) 8 bit are pushed in the {@link java.lang.Byte}.
      *
-     * @param value {@link Integer} to cast
-     * @return Casted {@link Byte}
+     * @param value {@link java.lang.Integer} to cast
+     * @return Casted {@link java.lang.Byte}
      */
     public static byte toByte(final int value) {
         return (byte)((int) (value & INT_TO_BYTE_MASK));
     }
 
     /**
-     * Casting a {@link Short} array to a {@link Byte} array.
+     * Casting a {@link java.lang.Short} array to a
+     * {@link java.lang.Byte} array.
      *
      * @see #toByte(short)
      *
-     * @param values Array of {@link Short} values to cast
-     * @return Array of casted {@link Byte} values
+     * @param values Array of {@link java.lang.Short} values to cast
+     * @return Array of casted {@link java.lang.Byte} values
      */
     public static byte[] toByteArray(final short[] values) {
         byte[] ret = new byte[values.length];
@@ -190,12 +200,13 @@ public abstract class Parse {
     }
 
     /**
-     * Casting a {@link Integer} array to a {@link Byte} array.
+     * Casting a {@link java.lang.Integer} array to a
+     * {@link java.lang.Byte} array.
      *
      * @see #toByte(int)
      *
-     * @param values Array of {@link Integer} values to cast
-     * @return Array of casted {@link Byte} values
+     * @param values Array of {@link java.lang.Integer} values to cast
+     * @return Array of casted {@link java.lang.Byte} values
      */
     public static byte[] toByteArray(final int[] values) {
         byte[] ret = new byte[values.length];
