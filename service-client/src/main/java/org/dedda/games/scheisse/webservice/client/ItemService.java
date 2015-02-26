6548ae7262bd7c6498805356b1bef07eb7dd04c1
Stephan Göppentin <dedda102@gmail.com>
2015-02-26 14:02:28 +0100

    fixed poms and added first wsdl stuff to service client module

---
+++ service-client/src/main/java/org/dedda/games/scheisse/webservice/client/ItemService.java
@@ -0,0 +1,13 @@
+package org.dedda.games.scheisse.webservice.client;
+
+import org.dedda.games.scheisse.webservice.ItemService_Service;
+
+/**
+ *
+ * @author dedda
+ */
+public class ItemService {
+
+    
+    
+}
