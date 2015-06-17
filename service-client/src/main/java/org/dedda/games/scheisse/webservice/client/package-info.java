efc2eef25f5cc94fcae1ec533f7928a1f14c12da
Stephan Göppentin <dedda102@gmail.com>
2015-06-17 21:26:36 +0200

    removed services

--- service-client/src/main/java/org/dedda/games/scheisse/webservice/client/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by sgoeppentin on 30.04.15.
- */
-package org.dedda.games.scheisse.webservice.client;
--- service/src/main/java/org/dedda/games/scheisse/service/ShopRequestService.java
+++
@@ -1,9 +0,0 @@
-package org.dedda.games.scheisse.service;
-
-/**
- * Created by dedda on 3/13/15.
- */
-public interface ShopRequestService {
-
-
-}
