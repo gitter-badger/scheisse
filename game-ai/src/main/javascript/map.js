1a344f55ab64b1e1bbfa67acb9dff8933fe4bcb1
Stephan Goeppentin <stephan.goeppentin@adition.com>
2015-07-02 11:14:19 +0200

    started implementing world and maps in nashorn

---
+++ game-ai/src/main/javascript/map.js
@@ -0,0 +1,13 @@
+function Map(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 0;
+    this.height = 0;
+    this.objects = [];
+    this.addObject = function(obj) {
+        this.objects.push(obj);
+    };
+    this.removeObject = function(obj) {
+        this.objects.removeObj(obj);
+    };
+}
---
+++ game-ai/src/main/javascript/world.js
@@ -0,0 +1,21 @@
+function World() {
+    this.maps = [];
+    this.addMap = function(map) {
+        var x = map.x;
+        var y = map.y;
+        if (typeof this.maps[x] !== 'array') {
+            this.maps[x] = [];
+        }
+        this.maps[x][y] = map;
+    };
+    this.removeMap(map) {
+        var x = map.x;
+        var y = map.y;
+        if (typeof this.maps[x][y] !== 'undefined') {
+            this.maps[x].splice(y, 1);
+        }
+    };
+    this.hasMapAt = function(x, y) {
+        return typeof this.maps[x][y] !== 'undefined';
+    };
+}
