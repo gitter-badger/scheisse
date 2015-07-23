2faa24fed028643eec1a7d87309838f25e5902b5
Stephan Goeppentin <dedda102@gmail.com>
2015-07-23 14:57:09 +0200

    added module for game events

---
+++ game-events/src/main/java/org/dedda/games/scheisse/events/GameCommonEvent.java
@@ -0,0 +1,31 @@
+package org.dedda.games.scheisse.events;
+
+/**
+ * Created by dedda on 7/23/15.
+ *
+ * @author dedda
+ */
+public class GameCommonEvent {
+
+    public static final int CODE_GENERAL = 0;
+    public static final int CODE_NEW_GAME_STARTED = 1;
+    public static final int CODE_GAME_LOADED = 2;
+    public static final int CODE_GAME_PAUSED = 3;
+    public static final int CODE_GAME_RESUMED = 4;
+
+    private final int code;
+    private final Object otherData;
+
+    public GameCommonEvent(final int code, final Object otherData) {
+        this.code = code;
+        this.otherData = otherData;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public Object getOtherData() {
+        return otherData;
+    }
+}
---
+++ game-events/src/main/java/org/dedda/games/scheisse/events/GameCommonEventListener.java
@@ -0,0 +1,12 @@
+package org.dedda.games.scheisse.events;
+
+/**
+ * Created by dedda on 7/23/15.
+ *
+ * @author dedda
+ */
+public interface GameCommonEventListener {
+
+    void gameCommonEvent(GameCommonEvent event);
+
+}
