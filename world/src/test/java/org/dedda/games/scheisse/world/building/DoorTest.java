87aac21658c98c877ed62de6b935a4e3235256b8
Stephan Göppentin <dedda102@gmail.com>
2015-07-25 16:56:54 +0200

    implemented door test

--- world/src/test/java/org/dedda/games/scheisse/world/building/DoorTest.java
+++ world/src/test/java/org/dedda/games/scheisse/world/building/DoorTest.java
@@ -2,19 +2,52 @@ package org.dedda.games.scheisse.world.building;
 
 import junit.framework.TestCase;
 import org.dedda.games.scheisse.tool.Direction;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.awt.Dimension;
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.dedda.games.scheisse.tool.Direction.DOWN;
+import static org.dedda.games.scheisse.tool.Direction.LEFT;
+import static org.dedda.games.scheisse.tool.Direction.UP;
+
+/**
+ * @author dedda
+ */
+@RunWith(Parameterized.class)
 public class DoorTest extends TestCase {
 
-    private Door door;
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {new Direction[]{DOWN, UP}, false, DOWN, true},
+            {new Direction[]{DOWN, UP}, false, LEFT, false},
+            {new Direction[]{DOWN, UP}, true, DOWN, false},
+            {new Direction[]{DOWN, UP}, true, LEFT, false},
+        });
+    }
 
+    @Parameter(value = 0)
+    public Direction[] passingDirections;
+
+    @Parameter(value = 1)
+    public boolean locked;
+
+    @Parameter(value = 2)
+    public Direction direction;
+
+    @Parameter(value = 3)
+    public boolean canPass;
+
+    @Test
     public void testCanPass() throws Exception {
-        door = new Door(
-            null,
-            null,
-            new Direction[]{Direction.LEFT, Direction.RIGHT},
-            false
-        );
-        assertTrue(door.canPass(Direction.LEFT, null));
-        assertTrue(door.canPass(Direction.RIGHT, null));
+        Door door = new Door(null, null, passingDirections, locked);
+        assertEquals(canPass, door.canPass(direction, null));
     }
 }
