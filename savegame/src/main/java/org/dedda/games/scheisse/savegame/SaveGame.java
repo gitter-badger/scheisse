ebae354099c703a53376e6d5238315ecf59ea794
Stephan Göppentin <dedda102@gmail.com>
2015-08-01 15:27:06 +0200

    added module for savegames

---
+++ savegame/src/main/java/org/dedda/games/scheisse/savegame/SaveGame.java
@@ -0,0 +1,12 @@
+package org.dedda.games.scheisse.savegame;
+
+/**
+ * Created by dedda on 8/1/15.
+ *
+ * @author dedda
+ */
+public class SaveGame {
+
+    private org.dedda.games.scheisse.player.Player player;
+
+}
